6cbd10c368fecc53d174d6470b0096d893b5b29d
Julia Szczuczko <julia@adcon.pl>
2023-11-30 13:47:43 +0100

    N Queen problem - backtracking solution

---
+++ Chess/Queen.cs
@@ -0,0 +1,169 @@
+﻿using System.Diagnostics;
+
+namespace Algorithms.Chess
+{
+    internal class Queen
+    {
+        // Stopwatch to measure the time taken to find solutions
+        private Stopwatch watch = new Stopwatch();
+
+        // Size of the chessboard (N x N)
+        private int boardSize;
+
+        // Array to store the column position of a queen in each row
+        private int[]? columnPositions;
+
+        // Arrays to check if a row or diagonal is available for placing a queen
+        private bool[]? rowAvailable, mainDiagonalAvailable, antiDiagonalAvailable;
+
+        // List to store all possible solutions
+        private List<int[]>? allSolutions;
+
+        // Solves the N-Queens problem for a single solution
+        public void SingleSolveQueens(int size)
+        {
+            InitializeBoard(size); // Initialize the board with the given size
+            watch.Start(); // Start the stopwatch
+            bool solutionFound = false;
+            TryPlaceQueen(0, ref solutionFound); // Start placing queens from the first row
+            watch.Stop(); // Stop the stopwatch after finding a solution or exhausting possibilities
+
+            if (solutionFound)
+            {
+                PrintSingleSolution(); // Print the found solution
+            }
+            else
+            {
+                Console.WriteLine("No solution found.");
+            }
+            Console.WriteLine("Time for single solution: " + watch.ElapsedMilliseconds + "ms\n");
+        }
+
+        // Solves the N-Queens problem for all possible solutions
+        public void SolveAllQueens(int size)
+        {
+            InitializeBoard(size); // Initialize the board with the given size
+            allSolutions = new List<int[]>(); // Initialize the list to store all solutions
+            watch.Start(); // Start the stopwatch
+            TryPlaceQueenAllSolutions(0); // Start placing queens from the first row
+            watch.Stop(); // Stop the stopwatch after finding all solutions
+
+            //PrintAllSolutions(); // Print all found solutions
+            Console.WriteLine("Time for all solutions: " + watch.ElapsedMilliseconds + "ms\n");
+        }
+
+        // Initializes the board and auxiliary arrays
+        private void InitializeBoard(int size)
+        {
+            this.boardSize = size;
+            columnPositions = new int[size]; // Initialize column positions
+            // Initialize availability of rows and diagonals
+            rowAvailable = Enumerable.Repeat(true, size).ToArray();
+            mainDiagonalAvailable = Enumerable.Repeat(true, 2 * size - 1).ToArray();
+            antiDiagonalAvailable = Enumerable.Repeat(true, 2 * size - 1).ToArray();
+        }
+
+        // Recursive method to place queens for a single solution
+        private void TryPlaceQueen(int row, ref bool solutionFound)
+        {
+            for (int col = 0; col < boardSize && !solutionFound; col++)
+            {
+                if (IsSafe(row, col)) // Check if it's safe to place a queen at the current position
+                {
+                    PlaceQueen(row, col); // Place the queen
+                    if (row < boardSize - 1)
+                    {
+                        TryPlaceQueen(row + 1, ref solutionFound); // Recursively try to place the next queen
+                        if (!solutionFound)
+                        {
+                            RemoveQueen(row, col); // Backtrack: Remove the queen and try the next position
+                        }
+                    }
+                    else
+                    {
+                        solutionFound = true; // All queens are placed successfully
+                    }
+                }
+            }
+        }
+
+        // Recursive method to place queens for all solutions
+        private void TryPlaceQueenAllSolutions(int row)
+        {
+            for (int col = 0; col < boardSize; col++)
+            {
+                if (IsSafe(row, col)) // Check if it's safe to place a queen at the current position
+                {
+                    PlaceQueen(row, col); // Place the queen
+                    if (row < boardSize - 1)
+                    {
+                        TryPlaceQueenAllSolutions(row + 1); // Recursively try to place the next queen
+                    }
+                    else
+                    {
+                        allSolutions.Add((int[])columnPositions.Clone()); // Add the current solution to the list
+                    }
+                    RemoveQueen(row, col); // Backtrack: Remove the queen and try the next position
+                }
+            }
+        }
+
+        // Checks if it's safe to place a queen at the given position
+        private bool IsSafe(int row, int col)
+        {
+            // Check if the row, main diagonal, and anti-diagonal are available
+            return rowAvailable[col] && mainDiagonalAvailable[row + col] && antiDiagonalAvailable[row - col + boardSize - 1];
+        }
+
+        // Places a queen at the given position
+        private void PlaceQueen(int row, int col)
+        {
+            columnPositions[row] = col; // Mark the column position of the queen
+            // Mark the row, main diagonal, and anti-diagonal as not available
+            rowAvailable[col] = false;
+            mainDiagonalAvailable[row + col] = false;
+            antiDiagonalAvailable[row - col + boardSize - 1] = false;
+        }
+
+        // Removes a queen from the given position
+        private void RemoveQueen(int row, int col)
+        {
+            // Mark the row, main diagonal, and anti-diagonal as available again
+            rowAvailable[col] = true;
+            mainDiagonalAvailable[row + col] = true;
+            antiDiagonalAvailable[row - col + boardSize - 1] = true;
+        }
+
+        // Prints the single solution found
+        private void PrintSingleSolution()
+        {
+            Console.WriteLine("Single Solution:");
+            PrintSolution(columnPositions); // Print the solution
+        }
+
+        // Prints all solutions found
+        private void PrintAllSolutions()
+        {
+            Console.WriteLine($"All Solutions (Total: {allSolutions.Count}):");
+            foreach (var solution in allSolutions)
+            {
+                PrintSolution(solution); // Print each solution
+                Console.WriteLine();
+            }
+        }
+
+        // Helper method to print a solution
+        private void PrintSolution(int[] solution)
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    // Print 'Q' for a queen and '.' for an empty space
+                    Console.Write(solution[i] == j ? "Q " : ". ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace Algorithms
             // - Permutacja vs. Kombinacja: Permutacja uwzględnia kolejność i używa wszystkich elementów, podczas gdy kombinacja ignoruje kolejność i uwzględnia jedynie wybór elementów.
 
 
-            //Permutation
+            // Permutation
             if (false)
             {
                 Console.WriteLine("Permutations");
@@ -44,7 +44,7 @@ namespace Algorithms
                 Console.WriteLine();
             }
 
-            //Combination
+            // Combination
             if (false)
             {
                 Console.WriteLine("Combinations");
@@ -54,7 +54,7 @@ namespace Algorithms
                 Console.WriteLine();
             }
 
-            //Variation
+            // Variation
             if (false)
             {
                 Console.WriteLine("Variations");
@@ -67,7 +67,7 @@ namespace Algorithms
                 Console.WriteLine();
             }
 
-            //Knight
+            // Zadanie 2 - problem skoczka
 
             // Metoda backtrackingu do rozwiązywania problemu trasy skoczka na szachownicy
             // Opiera się na próbie wszystkich możliwych ruchów skoczka, cofaniu się i ponownym próbowaniu nowych ścieżek
@@ -85,12 +85,55 @@ namespace Algorithms
             // 5. Jeśli znaleziono poprawną trasę (skoczek odwiedził każde pole na szachownicy dokładnie raz), zwracamy sukces.
             //    W przeciwnym razie, jeśli wszystkie możliwe ruchy zostały sprawdzone, ale nie znaleziono rozwiązania, zwracamy brak rozwiązania.
 
-            if (true)
+            if (false)
             {
                 Console.WriteLine("Knight Tour Problem");
                 Knight knight = new Knight();
                 knight.SolveKnightTour(6, 1, 1);
                 knight.FindAllSolutions(5, 1, 1);
+            }
+
+            // Zadanie 3 - problem N hetmanów
+
+            // Problem N Hetmanów polega na umieszczeniu N hetmanów na szachownicy N x N tak,
+            // aby żaden z hetmanów nie był zagrożony przez innego. Oznacza to, że żadne dwa
+            // hetmani nie mogą znajdować się w tej samej linii, kolumnie ani na tej samej przekątnej.
+
+            // Metoda backtrackingu (cofnij i spróbuj ponownie) jest techniką algorytmiczną używaną
+            // do znajdowania wszystkich (lub niektórych) możliwych konfiguracji w problemach kombinatorycznych.
+            // W kontekście problemu N Hetmanów, algorytm backtrackingu umieszcza hetmana w pierwszym możliwym
+            // miejscu w rzędzie, a następnie przechodzi do następnego rzędu. Jeśli w następnym rzędzie nie
+            // można bezpiecznie umieścić hetmana (tj. jest zagrożony przez innego hetmana), algorytm cofa się
+            // (usuwa hetmana) i próbuje umieścić hetmana w kolejnym miejscu w poprzednim rzędzie.
+
+            // Ten proces jest kontynuowany, aż hetman zostanie umieszczony w każdym rzędzie (co oznacza znalezienie
+            // rozwiązania) lub wszystkie możliwości zostaną wyczerpane bez znalezienia rozwiązania.
+
+            // Do śledzenia pozycji, gdzie można umieścić hetmana, używamy kilku zmiennych:
+            // 1. columnPositions: Tablica przechowująca pozycje kolumn hetmanów w każdym rzędzie.
+            //    Indeks tablicy reprezentuje rząd, a wartość w tym indeksie to kolumna, w której znajduje się hetman.
+            // 2. rowAvailable: Tablica boolowska określająca, czy dana kolumna jest wolna od innych hetmanów.
+            // 3. mainDiagonalAvailable i antiDiagonalAvailable: Tablice boolowskie do śledzenia dostępności przekątnych.
+            //    Przekątne są śledzone za pomocą sumy i różnicy indeksów rzędów i kolumn, co pozwala na efektywne
+            //    sprawdzenie, czy dana przekątna jest wolna.
+
+            // Podczas próby umieszczenia hetmana w danym rzędzie i kolumnie, algorytm najpierw sprawdza,
+            // czy kolumna oraz przekątne przechodzące przez tę pozycję są wolne. Jeśli tak, hetman jest umieszczany,
+            // a następnie algorytm przechodzi do następnego rzędu. Jeśli nie można umieścić hetmana, algorytm
+            // kontynuuje próby w kolejnych kolumnach tego samego rzędu.
+
+            // Gdy hetman jest umieszczany, odpowiadające mu wartości w tablicach rowAvailable,
+            // mainDiagonalAvailable i antiDiagonalAvailable są ustawiane na false, co oznacza, że te linie
+            // są już zajęte. Gdy algorytm cofa się (backtracking), wartości te są resetowane na true,
+            // co oznacza ponowne uwolnienie tych linii.
+
+            if (true)
+            {
+                Queen queen = new Queen();
+                queen.SingleSolveQueens(8);
+                queen.SolveAllQueens(14);
+                // granica "rozsądnego" czasu z uwzględnieniem wypisania wyników to 10
+                // dla samych obliczeń to 14 (zajmuję trochę ponad 6 s)
             }
         }
     }
